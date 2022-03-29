2a4306dc86b3b2a71ea6b75b0514cb7cc374ab14
gmetin <metinatamail@gmail.com>
2022-03-29 23:29:22 +0900

    periodic

--- CODES/sight_line_constructor.py
+++ CODES/sight_line_constructor.py
@@ -3,12 +3,13 @@ import numpy as np
 
 class Sightline:
 
-    def __init__(self,start,end,origin,num = 20): #start & end & origin are 3x1 arrays (x,y,z) each; num: entries per Mpc/h
+    def __init__(self,start,end,origin, periodic = False, num = 20): #start & end & origin are 3x1 arrays (x,y,z) each; num: entries per Mpc/h
         self.start = start
         self.end = end
         self.num = num
         self.origin = origin
         self.r = np.linalg.norm(self.end - self.start)
+        self.periodic = periodic
 
     def calc_los(self,res,delta):
         self.res = res
@@ -16,7 +17,11 @@ class Sightline:
         end =   ((self.end -self.origin)/res)
         #arr1 = np.pad(arr,(0,1),'constant') # if you want to add 0's at the end
         XYZ = np.floor(np.linspace(start, end, int(self.num*self.r))).astype(np.single)
-        los_delta = ndimage.map_coordinates(delta, XYZ.T, order=0, mode='constant',cval=-10.0)
+        self.los = []
+        if (self.periodic==True):
+            los_delta = ndimage.map_coordinates(delta, XYZ.T, order=0,  mode='grid-wrap')
+        else:
+            los_delta = ndimage.map_coordinates(delta, XYZ.T, order=0, mode='constant',cval=-10.0)
         self.los = los_delta
         return los_delta
     
